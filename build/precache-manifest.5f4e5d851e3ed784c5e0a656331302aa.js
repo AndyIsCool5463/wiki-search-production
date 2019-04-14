0cd6258efbe6e66aa0c398d2b196398d29f174f8
AndyIsCool5463 <www.andyiscool@outlook.com>
2019-04-13 22:01:38 -0700

    inital commit

---
+++ build/precache-manifest.5f4e5d851e3ed784c5e0a656331302aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "36fbbfb8668694ed64bb",
+    "url": "/static/js/main.768d81de.chunk.js"
+  },
+  {
+    "revision": "06405c56231eb12bd990",
+    "url": "/static/js/2.9f23e285.chunk.js"
+  },
+  {
+    "revision": "36fbbfb8668694ed64bb",
+    "url": "/static/css/main.5863d4c0.chunk.css"
+  },
+  {
+    "revision": "504ce33e7edce0232dd2ae87199ef827",
+    "url": "/index.html"
+  }
+];
